5f9488e6ea58bba94941eb8381b5e0c68ef49540
artor <artor.dev@gmail.com>
2022-07-01 14:54:38 +0200

    refactor

--- server.js
+++ server.js
@@ -1,55 +1,15 @@
 import express from "express";
-import data from './model/data.json' assert {type: 'json'};
-
+import data from "./model/data.json" assert { type: "json" };
 import cors from "cors";
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
-app.use(express.static('public'))
-app.use(cors())
-
-
-
-
-
-
+app.use(express.static("public"));
+app.use(cors());
 app.use(express.json());
 
+////////// ROUTES //////////
+app.get("/", (req, res) => res.send(data));
 
-
-app.get("/", (req, res) => {
-res.send(data)
- });
-// app.get("/", (req, res) => {
-//     console.log(process.cwd() + "/res/1.png" )
-
-
-        // res.sendFile("/res/1.png", {
-        //   root: process.cwd(), // directory name
-        // });
-    
-
-//   res.sendFile(path.join(__dirname, "./res/1.png"), {
-//     root: process.cwd(), // directory name
-//   });
-// });
-
-// app.get("/playlist", (req, res) => {
-//   res.sendFile("data/database.json", {
-//     root: process.cwd(), // directory name
-//   });
-// });
-
-// app.post("/video", (req, res) => {
-//   console.log(req.body);
-//   res.send("video added to playlist");
-// });
-
-// app.put("/video", (req, res) => {
-//   res.send("video updated");
-// });
-
-// app.delete("/video", (req, res) => {
-//   res.status(204).end();
-// });
-
-app.listen(8000, () => console.log("server is running on port 8000"));
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
